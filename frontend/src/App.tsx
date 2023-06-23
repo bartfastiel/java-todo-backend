285754003b029fdb61d706cc4a47ad9a2eb8c8fd
Ben-21 <bs@schaefer-inet.de>
2023-06-23 11:40:23 +0200

    dummy Daten implementiert

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+export default function App() {
+
+    const dummy = [
+        {
+            id: "1",
+            description: "wash the dishes",
+            status: "open",
+
+        },
+        {
+            id: "2",
+            description: "Touran in die Waschstraße fahren",
+            status: "done",
+
+        },
+        {
+            id: "c596",
+            description: "Getränke kaufen",
+            status: "DONE"
+       }
+    ]
+
+
+    return (
+        <>
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </>
+    )
+
+
+}
